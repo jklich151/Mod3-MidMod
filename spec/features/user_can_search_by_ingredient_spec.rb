696e1ed3514d26212b5826502d153a8ec57d244e
Jenny Klich <jennyklich@gmail.com>
2020-03-30 10:02:41 -0600

    Add progress

--- spec/features/user_can_search_by_ingredient_spec.rb
+++ spec/features/user_can_search_by_ingredient_spec.rb
@@ -5,14 +5,22 @@ feature "user can search by ingredient" do
   # When I visit "/"
     visit "/"
     # And I fill in the search form with "sweet potatoes"
-    select "sweet potatoes", from: :
+    select "sweet potatoes", from: :generalSearchInput
+    # (Note: Use the existing search form)
+
+    # And I click "Search"
+    click_on "Search"
+
+    expect(current_path).to eq("/foods")
+    # Then I should be on page "/foods"
+
+    # Then I should see a total of the number of items returned by the search.
+    expect(page).to have_content("totalHits": 32696)
+    # Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+    expect(page).to have_content("totalHits": 32696)
+
 end
 
-# (Note: Use the existing search form)
-# And I click "Search"
-# Then I should be on page "/foods"
-# Then I should see a total of the number of items returned by the search.
-# Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
 # And for each of the foods I should see:
 # - The food's GTIN/UPC code
 # - The food's description
